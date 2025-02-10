ae133731efb54efecd0211d1c0f1de6d97896695
hyde0038 <hyde0038@algonquinlive.com>
2025-02-10 10:30:49 -0500

    Removed class from lib, moved to main.dart, added required methods

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,23 @@
-import 'package:dart_exercise3/Students.dart' as Students;
+import 'dart:convert';
 
-void main(List<String> arguments) {}
+class Students {
+  List<Map<String, dynamic>> people;
+
+  Students(List<Map<String, dynamic>> this.people);
+
+  sort(String fieldName) {}
+  output() {}
+  plus(Map<String, dynamic> person) {}
+  remove(String fieldName) {}
+}
+
+void main() {
+  String json = '''
+[
+  {"id":1, "first":"Steve", "last":"Griffith", "email":"griffis@algonquincollege.com"},
+  {"id":2, "first":"Vladimir", "last":"Cezar", "email":"cezarv@algonquincollege.com"},
+  {"id":3, "first":"Tony", "last":"Davidson", "email":"davidst@algonquincollege.com"},
+  {"id":4, "first":"Adam", "last":"Robillard", "email":"robilla@algonquincollege.com"}
+]
+''';
+}
--- test/dart_exercise3_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_exercise3/Students.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
