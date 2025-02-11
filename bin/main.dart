385ad40446815f7f2f01b61386aedb4260e8c3ca
hyde0038 <hyde0038@algonquinlive.com>
2025-02-11 18:47:43 -0500

    Convert JSON into dart obj, pass object into instance of Wizards, call required methods

--- bin/main.dart
+++ bin/main.dart
@@ -1,24 +1,65 @@
 import 'dart:convert';
 
-class Students {
+class Wizards {
   List<Map<String, dynamic>> people;
 
-  Students(List<Map<String, dynamic>> this.people);
+  Wizards(List<Map<String, dynamic>> this.people);
 
-  sort(String fieldName) {}
-  output() {}
-  plus(Map<String, dynamic> person) {}
-  remove(String fieldName) {}
+  sort(String id) {
+    people.sort((wizardA, wizardB) =>
+        wizardA[id].toString().compareTo(wizardB[id].toString()));
+    output();
+  }
+
+  output() {
+    for (var wizard in people) {
+      print(wizard);
+    }
+    // Just adding a visual breaker cause it hurts my eyes
+    print('=-=-=-=-=-=-=-=-=-=');
+  }
+
+  plus(Map<String, dynamic> wizard) {
+    people.add(wizard);
+    output();
+  }
+
+  remove(String id) {
+    for (var wizard in people) {
+      wizard.remove(id);
+    }
+    output();
+  }
 }
 
 void main() {
   String orderOfWizards = '''
 [
-  {"id":1, "first":"Saruman", "last":"The White", "email":"burntheents@valinor.com"},
-  {"id":2, "first":"Gandalf", "last":"The Gray", "email":"TakeThatBalrog@valinor.com"},
-  {"id":3, "first":"Tony", "last":"Davidson", "email":"MushroomLover@valinor.com"},
-  {"id":4, "first":"Alatar", "last":"The Blue", "email":"GhostedAndGone@valinor.com"},
-  {"id":5, "first":"Pallando", "last":"The Blue", "email":"SeeYaLater@valinor.com"}
+  {"id":1, "name":"Saruman", "title":"the White", "location":"Isengard", "Maiar":"Aulë"},
+  {"id":2, "name":"Gandalf", "title":"the Gray", "location":"Ubiquitous", "Maiar":"Manwë and Varda"},
+  {"id":3, "name":"Radagast", "title":"the Brown", "location":"Rhosgobel", "Maiar":"Yavanna"},
+  {"id":4, "name":"Alatar", "title":"the Blue", "location":"Valinor", "Maiar":"Oromë"}
 ]
 ''';
+
+  List<Map<String, dynamic>> wizardList =
+      json.decode(orderOfWizards).cast<Map<String, dynamic>>();
+  var wizards = Wizards(wizardList);
+
+  print('Order of Wizards:');
+  wizards.output();
+
+  print('Adding a wizard to the order:');
+  wizards.plus({
+    "id": 5,
+    "first": "Pallando",
+    "title": "the Blue",
+    "location": "Valinor"
+  });
+
+  print('Sorting wizards by name:');
+  wizards.sort('name');
+
+  print('Removing the location from all wizards:');
+  wizards.remove('location');
 }
